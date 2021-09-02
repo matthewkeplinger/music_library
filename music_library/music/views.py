f5252e42fe94be7186f86734a7351fccc6c2d822
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-09-02 19:17:46 -0400

    Added get_song, get, and put to SongDetail (music/views.py), need delete endpoint

--- music_library/music/views.py
+++ music_library/music/views.py
@@ -23,3 +23,27 @@ class SongList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
+class SongDetail(APIView):
+        
+    def get_song(self, pk):
+        """Try to get a song by ID, if ID does not exist, then HTTP 404"""
+        try:
+            return Song.objects.get(pk=pk)
+        except Song.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        
+    def get(self,request,pk):
+        song=self.get_song(pk)
+        serializer=SongSerializer(song)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self,request,pk):
+        song=self.get_song(pk)
+        serializer = SongSerializer(song, data= request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
