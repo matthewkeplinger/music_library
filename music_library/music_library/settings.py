ac9fe0f73b8e06a2e862bb82e4ea4e85e0ae78db
Matthew Keplinger <matthew.keplinger85@gmail.com>
2021-09-10 13:00:50 -0400

    Added CORS references; spelled middle correctly to fix previous typo in middleware

--- music_library/music_library/settings.py
+++ music_library/music_library/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.milddleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'music_library.urls'
